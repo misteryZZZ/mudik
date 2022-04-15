2220544ce4f3ae113a1066a4a420133c8fde2495
Iqbal <you@example.com>
2022-04-15 14:39:44 +0700

    nextjs exportPathMap config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/cms': { page: '/cms' },
+      '/dashboard': { page: '/dashboard' },
+      '/dinkes': { page: '/dinkes' },
+    }
+  },
 }
 
 module.exports = nextConfig
