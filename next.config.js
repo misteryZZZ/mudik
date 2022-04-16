2c7af405dfbc2d47c59070cfd9e197f7dd588476
Iqbal <you@example.com>
2022-04-17 01:08:41 +0700

    delete exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/cms': { page: '/cms' },
-      '/dashboard': { page: '/dashboard' },
-      '/dinkes': { page: '/dinkes' },
-    }
-  },
+  // exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId } ) {
+  //   return {
+  //     '/': { page: '/' },
+  //     '/cms': { page: '/cms' },
+  //     '/dashboard': { page: '/dashboard' },
+  //     '/dinkes': { page: '/dinkes' },
+  //   }
+  // },
 }
 
 module.exports = nextConfig
